0bbe59ec97242efe4f4afe5d6a825528d18a6b45
Shengye <shengye472@gmail.com>
2025-03-18 19:00:57 +0100

    feat(#4): add architecture base

---
+++ shopflow/src/test/java/com/shopflow/ShopflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
